982d7a2f44e095b195ffc2aee0468d60db20b96d
innocent lou <bisonlou@gmail.com>
2020-01-08 08:38:28 +0300

    added auth0 boilerplate

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,19 +1,46 @@
 import React from 'react';
+import { useAuth0 } from "../../react-auth0-spa";
 import {
-    Toolbar, Typography, AppBar
+    Toolbar, Typography, AppBar, Button, Grid, withStyles
 } from '@material-ui/core';
 
-const NavBar = () => {
+import { NavBarStyles } from '../../styles/navBarStyles';
+
+const NavBar = ({ classes }) => {
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
     return (
+
         <AppBar>
-            <Toolbar>
-                <Typography variant="h6" noWrap>
-                    OAF Seed Packager
+            <Grid
+                container
+                justify="space-between"
+            >
+                <Toolbar>
+                    <Typography variant="h6" noWrap>
+                        OAF Seed Packager
                 </Typography>
-            </Toolbar>
+                </Toolbar>
+
+                <Grid item>
+                    {!isAuthenticated && (
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            className={classes.button}
+                            onClick={() => loginWithRedirect({})}>Log in</Button>
+                    )}
+
+                    {isAuthenticated && (
+                        <Button
+                            variant="contained"
+                            color="secondarys"
+                            className={classes.button}
+                            onClick={() => logout()}>Log out</Button>)}
+                </Grid>
+            </Grid>
         </AppBar>
     );
 }
 
-export default NavBar;
+export default withStyles(NavBarStyles)(NavBar);
---
+++ src/styles/navBarStyles.js
@@ -0,0 +1,9 @@
+export const NavBarStyles = ({
+    button: {
+        paddingLeft: 10,
+        paddingRight: 10,
+        marginTop: 10,
+        marginRight: 10,
+    }
+});
+
