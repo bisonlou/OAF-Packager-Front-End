91d6406ae5bd998170a283d5913f3f5b4885a3af
innocent lou <bisonlou@gmail.com>
2020-01-08 20:29:39 +0300

    add access token

--- src/services/farmerService/index.js
+++ src/services/farmerService/index.js
@@ -1,17 +1,29 @@
 import { BASE_URL } from '../../utils';
+import { useAuth0 } from "../../react-auth0-spa";
+
+
+const { getTokenSilently } = useAuth0();
+
 
 export const getFarmers = async () => {
-    const response = await fetch(`${BASE_URL}/farmers`);
+    const response = await fetch(`${BASE_URL}/farmers`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    });
     return await response.json();
 }
 
 export const saveFarmer = async farmer => {
-    console.log(farmer)
+    const token = await getTokenSilently();
+
     const response = await fetch(`${BASE_URL}/farmers`, {
         method: 'POST',
         body: JSON.stringify(farmer),
         headers: {
             'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
         }
     });
 
