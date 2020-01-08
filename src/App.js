ecddd775973fd42c5fdb3ae9364faaa8b3da65fe
innocent lou <bisonlou@gmail.com>
2020-01-08 14:02:50 +0300

    added loading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import Main from './layout/main';
+import { useAuth0 } from "./react-auth0-spa";
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route path='/' exact component={Main} />
-        </Switch>
-      </Router>
-    )
+const App = () => {
+
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
+
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact component={Main} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
